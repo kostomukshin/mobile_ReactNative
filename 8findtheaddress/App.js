deb0eac75e54b6747b9d277faad4103858c8e0ce
Vladan <vladan.marchenko@gmail.com>
2022-09-25 12:27:07 +0300

    8 & 10

---
+++ 8findtheaddress/App.js
@@ -0,0 +1,67 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
+export default function App() {
+  const [keyword, setKeyword] = useState('');
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0122,
+    longitudeDelta: 0.0121,
+  })
+
+const fetchMap = () => {
+  fetch(`https://www.mapquestapi.com/geocoding/v1/address?key=XPsObG5LAFRaGPTH442DwELtglDoA5Vy&location=${keyword}`)
+  .then(response => response.json())
+  .then(data => {
+      setRegion({
+        ...region,
+        latitude: data.results[0].locations[0].latLng.lat,
+        longitude: data.results[0].locations[0].latLng.lng,
+      })
+  })
+  .catch(err => Alert.alert('Error', err))
+}
+
+  return (
+    <View style={styles.container}>
+
+<TextInput
+    style={{fontSize: 20, marginTop: 70, marginLeft: 125, fontWeight: 'bold'}}
+    placeholder='Address to search'
+    onChangeText={text => setKeyword(text)}
+    />
+    <Button style={{marginBottom: 50}}
+    title = 'Search'
+    onPress = {fetchMap}
+    />      
+
+    <MapView style={{ flex: 1, marginBottom: 100}}
+      region={region}>
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region. longitude,
+          }}
+          />
+      </MapView>
+
+
+    
+
+
+
+
+
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
